c30f7f4b9a12598d973d388650b8e780cf53296a
Do Van Duc <ducdv@ssi.com.vn>
2023-06-20 08:41:42 +0700

    fix: remove

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	market: {
 	HubUrl: "wss://fc-data.ssi.com.vn/",
 	ApiUrl: "https://fc-data.ssi.com.vn/",
-	ConsumerId: "2c7c511d4eb940d6a8b09aaef5711654",
-	ConsumerSecret: "27a3675fafe846cd851307963f6efd35"
+	ConsumerId: "",
+	ConsumerSecret: ""
 	},
 };
